da68f68c88336c5a40b397816971fe6de6a7e692
Leandro Biciato <leandrobiciato58@gmail.com>
2019-11-29 16:54:18 -0300

    fiexed layout 2 divs

--- app/Http/Controllers/SpreadsheetController.php
+++ app/Http/Controllers/SpreadsheetController.php
@@ -49,7 +49,7 @@ class SpreadsheetController extends Controller
         } catch(Exception $e) {
             return $e;
         } finally {
-            Mail::to('leandrobiciato58@gmail.com')
+            Mail::to('rafaelh_rosario@hotmail.com')
                     ->send(new PedidoCriado(storage_path(
                         'app/pedidos-excel/'
                         .$client
